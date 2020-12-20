4ebb14a0e2a5ce7d8a333306e7cfa757b6327a1b
Sumit Chougule <sumitchougule@Sumits-iMac.local>
2020-12-19 19:47:56 -0800

    arrays

---
+++ arrays.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+
+# declaring an array
+my @arr = (1,2,3,4);
+
+# print "$command\n";
+print "@arr\n";
+
+# printing elements of an array individually
+print "$arr[0]\n";
+print "$arr[1]\n";
+print "$arr[2]\n";
+print "$arr[3]\n";
+
+# arrays of strings
+my @words = ("one","two","three","four");
+print "@words\n";
+
+# add element to array
+$word[4] = "five";
+print "@words\n";
+
+exit;
